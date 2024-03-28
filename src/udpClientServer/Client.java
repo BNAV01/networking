742614507c4c88993c200b6af1d1e0d48a0b7af9
BNAV01 <bnavarretem@uft.edu>
2024-03-28 05:48:43 -0300

    Cambios y optimizacion del protocolo UDP

--- src/udpClientServer/Client.java
+++ src/udpClientServer/Client.java
@@ -1,4 +1,51 @@
 package udpClientServer;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
+
+    private static final int PORT = 8082;
+
+    public static void main(String[] args) {
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            accessServer(host);
+        } catch (UnknownHostException unknownHostException) {
+            System.out.println("ERROR : " + unknownHostException.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static void accessServer(InetAddress host) {
+        try (DatagramSocket datagramSocket = new DatagramSocket()) {
+            Scanner userEntry = new Scanner(System.in);
+            String message = "", response = "";
+            do {
+                System.out.println("Ingresa Mensaje : ");
+                message = userEntry.nextLine();
+                if (!message.equals("***CLOSE***")) {
+                    byte[] buffer = message.getBytes();
+                    DatagramPacket outPacket = new DatagramPacket(buffer, buffer.length, host, PORT);
+                    datagramSocket.send(outPacket);
+
+                    buffer = new byte[256];
+                    DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+                    datagramSocket.receive(inPacket);
+
+                    response = new String(inPacket.getData(), 0, inPacket.getLength());
+                    System.out.println("\nRespuesta del servidor > " + response);
+                }
+            } while (!message.equals("***CLOSE***"));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            System.out.println("ERROR: " + ioException.getMessage());
+        } finally {
+            System.out.println("\n* Terminando Conexion ... *");
+        }
+    }
 }
--- src/udpClientServer/Server.java
+++ src/udpClientServer/Server.java
@@ -1,4 +1,44 @@
 package udpClientServer;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Server {
+
+    private static final int PORT = 8082;
+
+    public static void main(String[] args) {
+        System.out.println("Abriendo puerto. . . \n");
+        try (DatagramSocket datagramSocket = new DatagramSocket(PORT)) {
+            handleClient(datagramSocket);
+        } catch (SocketException socketException) {
+            System.out.println("IMPOSIBLE ACCEDER AL PUERTO");
+            System.out.println(socketException.getMessage());
+            System.exit(1);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public static void handleClient(DatagramSocket datagramSocket) throws IOException {
+        byte[] buffer = new byte[256];
+        int numMessages = 0;
+
+        while (true) {
+            DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(inPacket);
+
+            String messageIn = new String(inPacket.getData(), 0, inPacket.getLength());
+            System.out.println("Mensaje recibido");
+            numMessages++;
+
+            String messageOut = "Mensaje " + numMessages + " : " + messageIn;
+            DatagramPacket outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(),
+                    inPacket.getAddress(), inPacket.getPort());
+            datagramSocket.send(outPacket);
+            System.out.println("Mensaje : " + messageOut);
+        }
+    }
 }
